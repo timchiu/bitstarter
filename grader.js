bf16d188284574fe3489066ebe86a7505d7c100e
Tim Chiu <timchiu@yahoo.com>
2013-07-12 09:25:30 +0000

    added support for url in grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +66,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_address>', 'URL path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result) {
+	    if (result) {
+		fs.writeFileSync(HTMLFILE_DEFAULT)
+	    }
+	});
+    }
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
